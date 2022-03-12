af0a4e731f2e4f8ee43ee8ec054df8ffa666a143
aleem-ahmed <w3st.io2021@gmail.com>
2022-03-12 16:13:41 -0500

    Notes Should be above any line of code

--- DelegateCall/DelegateCall.sol
+++ DelegateCall/DelegateCall.sol
@@ -1,11 +1,11 @@
-pragma solidity ^0.8.3;
-
-/// @notice delegatecall is a low level function similar to call. When contract A 
+/// Delegatecall is a low level function similar to call. When contract A 
 /// executes delegatecall to contract B, B's code is executed with contract A'storage,
 /// msg.sender and msg.value.
 
 /// When a contract makes a function call using delegatecall it loads the function code
 /// from another contract and executed it as if it were its own code
+
+pragma solidity ^0.8.3;
 
 contract ContractB {
 	uint public num;
