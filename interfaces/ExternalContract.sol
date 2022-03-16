fbfb6819f419120e8c8dc5d4190da1c2a2f07df7
aleem-ahmed <w3st.io2021@gmail.com>
2022-03-15 20:29:33 -0400

    Improved

--- interfaces/ExternalContract.sol
+++ interfaces/ExternalContract.sol
@@ -1,6 +1,7 @@
 // This contract servces as an example contract that I want to create an interface for
 // I do not want to copy and past this code to my smart contract so I will create an
 // Interface for it.
+
 pragma solidity ^0.8.3;
 
 contract Counter {
